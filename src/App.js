817a8fff5f4735a3e85ef6d41ef5f909e6afba85
yarimu <yhli0320@naver.com>
2024-06-10 22:02:12 +0900

    init: 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Random.pickNumberInRange(1, 10); // 1
+  }
 }
+
+const one = new App();
+one.play();
 
 export default App;
